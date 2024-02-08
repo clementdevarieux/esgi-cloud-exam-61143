301b685bacae514a188319d64918d94d8808e289
Clement Devarieux <devarieux.clement@gmail.com>
2024-02-08 10:55:32 +0100

    still trying

--- db.client.js
+++ db.client.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize')
-require('dotenv').config();
 
-const url_database_var = process.env.url_database
 // database
 const sequelize = new Sequelize(
   'postgres://db_postgre_exo_1_61143_user:je8IhihaJ1bgL1TiGF2wBSEHHL8wqozX@dpg-cn29upn109ks7394k0r0-a.frankfurt-postgres.render.com/db_postgre_exo_1_61143',
